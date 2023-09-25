275b2ebe76bcb79bbbf0f7343a91504e3f056e19
manel <manel.vfranques@gmail.com>
2023-09-25 17:08:42 +0200

    changed variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ const m2dxOptions = {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nebulix.unfolding.io",
+  site: "https://bd7pc.netlify.app",
   integrations: [
     mdx({}),
     sitemap(),
